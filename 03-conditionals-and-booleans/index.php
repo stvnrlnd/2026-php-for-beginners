c006e23df49512d351bb083d71740043bc094741
Steven Roland <steven@segment.team>
2026-07-13 15:46:26 -0500

    Add index file

---
+++ 03-conditionals-and-booleans/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<style>
+	    body {
+			display: grid;
+			place-items: center;
+			height: 100vh;
+			margin: 0;
+			font-family: sans-serif;
+		}
+	</style>
+</head>
+<body>
+    <h1>
+        You have read "Dark Matter."
+    </h1>
+</body>
+</html>
