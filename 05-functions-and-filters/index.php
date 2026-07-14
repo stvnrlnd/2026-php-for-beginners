b99dfd039fdc98ab9ccfdbaea76e32e084d22fc8
Steven Roland <steven@segment.team>
2026-07-14 12:34:56 -0500

    Add fucntion to filter books by author name

--- 05-functions-and-filters/index.php
+++ 05-functions-and-filters/index.php
@@ -1,5 +1,34 @@
 <?php
-    // Let's get started
+    $books = [
+        [
+            'name' => 'Dark Matter',
+            'author' => 'Blake Crouch',
+            'releaseYear' => 2020,
+            'purchaseUrl' => 'https://example.com/dark-matter',
+        ],
+        [
+            'name' => 'The Great Gatsby',
+            'author' => 'F. Scott Fitzgerald',
+            'releaseYear' => 1925,
+            'purchaseUrl' => 'https://example.com/the-great-gatsby',
+        ],
+        [
+            'name' => '1984',
+            'author' => 'George Orwell',
+            'releaseYear' => 1949,
+            'purchaseUrl' => 'https://example.com/1984',
+        ],
+    ];
+
+    function filterByAuthor($books, $author) {
+        $filteredBooks = [];
+        foreach ($books as $book) {
+            if ($book['author'] === $author) {
+                $filteredBooks[] = $book;
+            }
+        }
+        return $filteredBooks;
+    }
 ?>
 
 <!DOCTYPE html>
@@ -10,6 +39,15 @@
     	<title>Document</title>
     </head>
     <body>
+        <h1>Recommended Books</h1>
 
+        <ul>
+            <?php foreach (filterByAuthor($books, 'Blake Crouch') as $book) : ?>
+                <li>
+                    <?= $book['name']; ?> by <?= $book['author']; ?>
+                    (<a href="<?= $book['purchaseUrl']; ?>">purchase</a>)
+                </li>
+            <?php endforeach; ?>
+        </ul>
     </body>
 </html>
